9ac1ac7ed49d3e5ccec0a64c4dee3c2c0cc25bbf
arthurits <simply_alfy@hotmail.com>
2024-11-10 11:18:11 +0100

    Add code for drag and drop

--- AsciiArt/Views/MainPage.xaml.cs
+++ AsciiArt/Views/MainPage.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using AsciiArt.ViewModels;
-
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media.Imaging;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Storage;
 
 namespace AsciiArt.Views;
 
@@ -13,7 +16,29 @@ public sealed partial class MainPage : Page
 
     public MainPage()
     {
-        ViewModel = App.GetService<MainViewModel>();
         InitializeComponent();
+        ViewModel = App.GetService<MainViewModel>();
+        
+    }
+
+    private void Grid_DragOver(object sender, DragEventArgs e)
+    {
+        e.AcceptedOperation = DataPackageOperation.Copy;
+    }
+
+    private async void Grid_Drop(object sender, DragEventArgs e)
+    {
+        if (e.DataView.Contains(StandardDataFormats.StorageItems))
+        {
+            var items = await e.DataView.GetStorageItemsAsync();
+            if (items.Count > 0)
+            {
+                var storageFile = items[0] as StorageFile;
+                var bitmapImage = new BitmapImage();
+                bitmapImage.SetSource(await storageFile.OpenAsync(FileAccessMode.Read));
+                // Set the image on the main page to the dropped image
+                Image.Source = bitmapImage;
+            }
+        }
     }
 }
